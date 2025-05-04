9a47fb926be7e73c20e6bf407e84ef3e802a58b4
KimB <kbaring@proweaver.net>
2025-05-04 17:01:40 +0800

    added auth to mongodb

--- config/database.php
+++ config/database.php
@@ -33,6 +33,8 @@ return [
         'mongodb' => [
             'driver' => 'mongodb',
             'dsn' => env('DB_URI'),
+            'username' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
             'database' => env('DB_DATABASE'),
         ],
     ],
